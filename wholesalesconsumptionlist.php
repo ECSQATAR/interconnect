bd55d3fe077dc960a25bf9bca9952384a76894f7
narasimha_seseti <snmurty99@gmail.com>
2016-05-19 13:47:59 +0400

    newton

--- wholesalesconsumptionlist.php
+++ wholesalesconsumptionlist.php
@@ -151,14 +151,38 @@ if(isset($_GET['action']) && $_GET['action']=='lock'){
 
 ?>
 
-<h1> Consumptions List </h1>
+
 
 <form role="form" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
 <div class="row">
-    <a href="index.php"> Back to home Page </a> | &nbsp;  <a href="importconsumeddata.php"> Import Consumption data </a> ;&nbsp; 
+  <div class="col-md-3"> &nbsp; </div>  <a href="index.php"> Back to home Page </a> &nbsp; 
 || <a href="prefixmasterlist.php"> Manage Prefix </a> || <a href="companieslist.php"> Manage Company</a> || <a href="addcompany.php"> Add Company </a>
 </div>
+
+<div class="row">
+<div class="col-md-3">
+ Invoices -> 
+</div> 
+ <a href="wholesaleinvoiceslist.php"> Manage Invoices. </a>  || &nbsp;&nbsp; <a href="importinvoicesdata.php"> Import invoices data </a>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-3">
+Consumption Reports ->
+</div>
+ <a href="wholesalesconsumptionlist.php"> Manage  Consumption Reports </a> || &nbsp;&nbsp; <a href="importconsumeddata.php"> Import Consumption data </a>
+</div>
+
+<div class="row">
+&nbsp; <br/>
+</div>
+
+
+
+<h1> Consumption Reports </h1>
 
 <div class="row">
 
